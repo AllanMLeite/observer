e706624ace3d149d450c0680155e8b755396e017
Allan <ilegra.allan>
2019-03-18 19:32:55 -0300

    remove observer

--- src/test/java/br/com/dp/observer/NewsAgencyTest.java
+++ src/test/java/br/com/dp/observer/NewsAgencyTest.java
@@ -9,16 +9,28 @@ public class NewsAgencyTest {
 
 	@Test
 	public void shouldPropagateNews() {
-			NewsAgency observable = new NewsAgency();
-			NewsChannel observer = new NewsChannel();
-			assertNull(observer.getNews());
-			
-			observable.addObserver(observer);
-			
-			observable.propagate("cacildis");
-			assertEquals("cacildis", observer.getNews());
-			
-			observable.propagate("newzis");
-			assertEquals("newzis", observer.getNews());
+		NewsAgency observable = new NewsAgency();
+		NewsChannel observer = new NewsChannel();
+		assertNull(observer.getNews());
+
+		observable.addObserver(observer);
+
+		observable.propagate("cacildis");
+		assertEquals("cacildis", observer.getNews());
+
+		observable.propagate("newzis");
+		assertEquals("newzis", observer.getNews());
+	}
+
+	@Test
+	public void shouldRemoveObserver() {
+		NewsAgency observable = new NewsAgency();
+		assertEquals(0, observable.countObservers());
+		NewsChannel observer = new NewsChannel();
+		observable.addObserver(observer);
+		assertEquals(1, observable.countObservers());
+
+		observable.deleteObserver(observer);
+		assertEquals(0, observable.countObservers());
 	}
 }
